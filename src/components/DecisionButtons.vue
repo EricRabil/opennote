efb7251d9963d4a426ea3cbc813a8dbf93a80d90
Eric Rabil <ericjrabil@gmail.com>
2020-02-23 01:17:43 -0500

    Add decisionbuttons component

---
+++ src/components/DecisionButtons.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="controls">
+        <div :class="['control', `control-${confirmStyle}`]" @click="confirm">
+            {{confirmText}}
+        </div>
+        <div class="control control-neutral" @click="cancel">
+            {{cancelText}}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class ConfirmationModal extends Vue {
+    @Prop()
+    cancel: () => any;
+
+    @Prop()
+    confirm: () => any;
+
+    @Prop({ default: 'Cancel' })
+    confirmText: string;
+
+    @Prop({ default: 'Confirm' })
+    cancelText: string;
+
+    @Prop({ default: 'neutral' })
+    confirmStyle: string;
+}
+</script>
+
+<style lang="scss">
+.modal {
+    .control:not(:last-child) {
+        margin-right: 5px;
+    }
+    .control {
+        @extend %bgAlt3;
+        color: white;
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+        text-align: center;
+        font-size: 11px;
+        flex-grow: 1;
+        transition: background-color 0.0625s linear;
+
+        &:hover {
+            @extend %bgAlt4;
+        }
+
+        &:active {
+            @extend %bgAlt5;
+        }
+
+        &.control-danger {
+            @extend %bgRed;
+            &:hover {
+                @extend %bgRedAlt;
+            }
+
+            &:active {
+                @extend %bgRedAlt1;
+            }
+        }
+    }
+    .controls {
+        display: flex;
+        flex-flow: row;
+        text-transform: uppercase;
+    }
+}
+</style>
