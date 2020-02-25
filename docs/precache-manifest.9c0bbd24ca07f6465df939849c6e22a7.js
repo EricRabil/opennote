da73f5b4f50814e8a4cc7ddadb161425b9528d13
Eric Rabil <ericjrabil@gmail.com>
2020-02-25 00:15:13 -0500

    1.1.0

---
+++ docs/precache-manifest.9c0bbd24ca07f6465df939849c6e22a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028d789d8d8fd5b6313a729bed25d01a",
+    "url": "/opennote/9d665a25ffefc7c6c151.worker.js"
+  },
+  {
+    "revision": "aabe836b11044e1769f8",
+    "url": "/opennote/css/app.d750c262.css"
+  },
+  {
+    "revision": "268784e69987ef1ee75ba2ef902a5635",
+    "url": "/opennote/index.html"
+  },
+  {
+    "revision": "aabe836b11044e1769f8",
+    "url": "/opennote/js/app.22fdbc7f.js"
+  },
+  {
+    "revision": "e23ab81b7a05840bfb8b",
+    "url": "/opennote/js/chunk-265e34ea.fbd738e5.js"
+  },
+  {
+    "revision": "55a41eaa8b6b458fc43e",
+    "url": "/opennote/js/chunk-vendors.1fd958bd.js"
+  },
+  {
+    "revision": "3321970ec45093ab9390731e658c3470",
+    "url": "/opennote/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/opennote/robots.txt"
+  }
+]);
