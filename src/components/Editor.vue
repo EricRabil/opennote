a2f86dc846414a187c3bfb6ceeca32390149eeb9
Eric Rabil <ericjrabil@gmail.com>
2020-02-25 00:10:25 -0500

    Raw tool is kill

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -331,7 +331,7 @@ export default class Editor extends Vue {
         }),
         checklist: Checklist,
         code: Code,
-        raw: Raw,
+        // raw: Raw,
         delimiter: Delimiter,
         embed: Embed,
         inlineCode: InlineCode,
